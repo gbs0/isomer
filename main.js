31121a1dc7de9659054f0d669854ff49468e0a58
gabriel schiavo <gabriel@lewagon.org>
2019-08-19 23:22:16 -0300

    add some code notations

--- main.js
+++ main.js
@@ -8,14 +8,20 @@ var blue = new Color(50, 60, 160);
 var black = new Color(20, 20, 20);
 
 iso.add(Shape.Prism(Point.ORIGIN, 10, 10, 1));
-iso.add(Shape.Pyramid(Point(0, 2, 1)), black);
+// iso.add(Shape.Pyramid(Point(0, 2, 1)), black);
 iso.add(Shape.Prism(Point(2, 0, 1)), blue);
-iso.add(Shape.Prism(Point(3, 0, 1)), blue);
-iso.add(Shape.Prism(Point(4, 0, 1)), red);
+iso.add(Shape.Prism(Point(1, 0, 1)), blue);
+iso.add(Shape.Prism(Point(1, 1, 1)), blue);
+iso.add(Shape.Prism(Point(1, 0, 2)), blue);
+iso.add(Shape.Prism(Point(6, 2, 0)), red);
+iso.add(Shape.Prism(Point(6, 3, 0)), red);
+
+iso.add(Shape.Prism(Point(5, 0, 1)), red);
 
 var step;
 for (let step = 0; step < 5; step++) {
   // Runs 5 times, with values of step 0 through 4.
 	iso.add(Shape.Prism(Point((step * -1), step, 0)), black);  
+	iso.add(Shape.Prism(Point((step * -1 / 2), step, 5)), black);  
 	console.log(step);
 }
